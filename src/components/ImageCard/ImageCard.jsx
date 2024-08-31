518fb7c9b45a9e2a55822714f93b5e06b2a7cfc1
Kate <159571202+turchynska@users.noreply.github.com>
2024-08-31 07:36:29 -0400

    finish

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -2,11 +2,12 @@ import style from './ImageCard.module.css';
 
 const ImageCard = ({ image, onOpen }) => {
   return (
-    <div className={style.card} onClick={onOpen}>
+    <div className={style.card}>
       <img 
         className={style.img}
+        onClick={onOpen}
         src={image.urls.small} 
-        alt={image.description}
+        alt={image.description  || "Image description"}
       />
     </div>
   );
